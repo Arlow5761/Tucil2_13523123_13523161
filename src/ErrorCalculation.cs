93b0766b6aafb949c72c37fabf1cfe307608d361
Arlow5761 <arlow5761@gmail.com>
2025-03-26 18:42:22 +0700

    refactor: change type of pixel to multidimensional array

--- src/ErrorCalculation.cs
+++ src/ErrorCalculation.cs
@@ -5,5 +5,5 @@ using System.Drawing;
 public abstract class ErrorCalculator
 {
     public abstract string Name { get; }
-    public abstract double CalculateError(Color[][] pixels);
+    public abstract double CalculateError(Color[,] pixels);
 }
