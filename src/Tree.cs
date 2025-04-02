b4c1e80a4c9cf1f6628562d1cbaf15082c2ecf9f
Arlow5761 <arlow5761@gmail.com>
2025-04-03 01:34:57 +0700

    refactor: change implementation of quadtree to use heap

--- src/Tree.cs
+++ src/Tree.cs
@@ -13,65 +13,88 @@ public class QuadTree
         this.sourceImage = source;
         this.errorCalculator = errorCalculator;
         this.rootNode = new Node() { content = new ImageRegion(new Region2Int(0, 0, source.Width - 1, source.Height - 1)) };
-        this.leafNodes = new List<Node>([rootNode]);
-
-        int i = 0;
-        while (i < leafNodes.Count)
+        
+        LinkedList<Node> tempLeaves = new LinkedList<Node>([rootNode]);
+        LinkedListNode<Node>? currentNode = tempLeaves.First;
+        while (currentNode is not null)
         {
-            Region2Int currentRegion = leafNodes[i].content.region;
+            Node node = currentNode.ValueRef;
+            Region2Int currentRegion = node.content.region;
 
             if (currentRegion.area >= 4 * minBlockSize && currentRegion.size.x > 1 && currentRegion.size.y > 1)
             {
-                leafNodes[i].children[0] = new Node()
+                node.children[0] = new Node()
                 {
-                    parent = leafNodes[i],
+                    parent = node,
                     content = new ImageRegion(new Region2Int(currentRegion.start, currentRegion.start + currentRegion.size / 2 - new Vector2Int(1, 1)))
                 };
 
-                leafNodes[i].children[1] = new Node()
+                node.children[1] = new Node()
                 {
-                    parent = leafNodes[i],
+                    parent = node,
                     content = new ImageRegion(new Region2Int(currentRegion.start + new Vector2Int(currentRegion.size.x / 2, 0), currentRegion.start + new Vector2Int(currentRegion.size.x - 1, currentRegion.size.y / 2 - 1)))
                 };
 
-                leafNodes[i].children[2] = new Node()
+                node.children[2] = new Node()
                 {
-                    parent = leafNodes[i],
+                    parent = node,
                     content = new ImageRegion(new Region2Int(currentRegion.start + new Vector2Int(0, currentRegion.size.y / 2), currentRegion.start + new Vector2Int(currentRegion.size.x / 2 - 1, currentRegion.size.y - 1)))
                 };
 
-                leafNodes[i].children[3] = new Node()
+                node.children[3] = new Node()
                 {
-                    parent = leafNodes[i],
+                    parent = node,
                     content = new ImageRegion(new Region2Int(currentRegion.start + currentRegion.size / 2, currentRegion.end))
                 };
 
-                leafNodes.Add(leafNodes[i].children[0]!);
-                leafNodes.Add(leafNodes[i].children[1]!);
-                leafNodes.Add(leafNodes[i].children[2]!);
-                leafNodes.Add(leafNodes[i].children[3]!);
+                tempLeaves.AddLast(node.children[0]!);
+                tempLeaves.AddLast(node.children[1]!);
+                tempLeaves.AddLast(node.children[2]!);
+                tempLeaves.AddLast(node.children[3]!);
 
-                leafNodes.RemoveAt(i);
-                
+                currentNode = currentNode.Next;
+
+                tempLeaves.Remove(currentNode!.Previous!);
+
                 continue;
             }
 
-            i++;
+            currentNode = currentNode.Next;
         }
 
-        for (i = 0; i < leafNodes.Count; i++)
+        this.leafNodes = tempLeaves.ToList();
+
+        for (int i = 0; i < leafNodes.Count; i++)
         {
             leafNodes[i].content.error = errorCalculator.CalculateError(leafNodes[i].content.region);
         }
 
-        SortLeaves(0, leafNodes.Count - 1);
+        SortLeaves();
     }
 
     public ImageRegion Pop()
     {
         Node poppedNode = leafNodes[0];
 
-        leafNodes.RemoveAt(0);
+        leafNodes[0] = leafNodes[leafNodes.Count - 1];
+        leafNodes.RemoveAt(leafNodes.Count - 1);
+
+        for (int i = 2; i <= leafNodes.Count; i *= 2)
+        {
+            if (i < leafNodes.Count && leafNodes[i].content.error < leafNodes[i - 1].content.error)
+            {
+                i++;
+            }
+
+            if (leafNodes[i / 2 - 1].content.error > leafNodes[i - 1].content.error)
+            {
+                (leafNodes[i / 2 - 1], leafNodes[i - 1]) = (leafNodes[i - 1], leafNodes[i / 2 - 1]);
+            }
+            else
+            {
+                break;
+            }
+        }
         
         if (poppedNode.parent is null)
         {
@@ -107,64 +130,27 @@ public class QuadTree
     private Node rootNode;
     private List<Node> leafNodes;
 
-    private void SortLeaves(int start, int end)
+    private void SortLeaves()
     {
-        if (start >= end) return;
-
-        int i = start;
-        int j = end;
-
-        double pivotError = leafNodes[i].content.error;
-
-        while (i <= j)
+        for (int i = leafNodes.Count - 1; i > 0; i--)
         {
-            while (i <= j && leafNodes[i].content.error < pivotError)
+            int parentIndex = (i + 1) / 2 - 1;
+
+            if (leafNodes[parentIndex].content.error > leafNodes[i].content.error)
             {
-                i++;
+                (leafNodes[parentIndex], leafNodes[i]) = (leafNodes[i], leafNodes[parentIndex]);
             }
-
-            while (i <= j && leafNodes[j].content.error > pivotError)
-            {
-                j--;
-            }
-
-            if (i > j) break;
-
-            (leafNodes[i], leafNodes[j]) = (leafNodes[j], leafNodes[i]);
-            i++;
-            j--;
         }
-        
-        SortLeaves(start, j);
-        SortLeaves(i, end);
     }
 
     private void InsertLeafNode(Node node)
     {
-        int i = 0;
-        int j = leafNodes.Count;
-        int pivot = (i + j) / 2;
+        leafNodes.Add(node);
 
-        while (i + 1 < j)
+        for (int i = leafNodes.Count; i > 1 && leafNodes[i - 1].content.error < leafNodes[i / 2 - 1].content.error; i /= 2)
         {
-            pivot = (i + j) / 2;
-
-            if (node.content.error == leafNodes[pivot].content.error)
-            {
-                leafNodes.Insert(pivot, node);
-                return;
-            }
-            else if (node.content.error > leafNodes[pivot].content.error)
-            {
-                i = pivot;
-            }
-            else if (node.content.error < leafNodes[pivot].content.error)
-            {
-                j = pivot;
-            }
+            (leafNodes[i / 2 - 1], leafNodes[i - 1]) = (leafNodes[i / 2 - 1], leafNodes[i - 1]);
         }
-
-        leafNodes.Insert(pivot, node);
     }
 
     private class Node
